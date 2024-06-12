a41cce0a22f89cf807f7869bfce1024f40449bae
Chris Nielsen <nielsen.chris@gmail.com>
2024-06-12 19:41:19 -0400

    Repair unit tests.

--- src/app/http/api/PathToTemplate.test.ts
+++ src/app/http/api/PathToTemplate.test.ts
@@ -2,7 +2,7 @@ import PathToTemplate from '#app/http/api/PathToTemplate.js';
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 
-describe(PathToTemplate.name, () => {
+describe("PathToTemplate", () => {
 	it('creates regular expressions to match path templates', () => {
 		// Arrange
 		const path = '/api/v1/users/{userId}/posts/{post-id}';
@@ -13,9 +13,9 @@ describe(PathToTemplate.name, () => {
 		const { identifiers, pattern } = PathToTemplate(path) ?? {};
 
 		// Assert
-		assert.equal(pattern, expected);
+		assert.strictEqual(pattern?.source, expected.source);
 
-		assert.equal(identifiers, new Map([
+		assert.deepStrictEqual(identifiers, new Map([
 			['id0', 'userId'],
 			['id1', 'post-id']
 		]));
